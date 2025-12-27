0af9bd1b84a58dc4a6fa9c6ef658bd5afc155033
Vladimir-cloudj <dmitry.bazyk@yandex.ru>
2025-12-27 14:07:07 +0400

    feat: logic validator algoritm

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,29 @@
-// TODO: write code here
+
+
+import './css/style.css';
+import './components/card-validator-widget/card-validator-widget.css';
+import './js/app';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // comment this to pass build
-const unusedVariable = "variable";
+// const unusedVariable = "variable";
 
 // for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
+// export default function demo(value) {
+//   return `Demo: ${value}`;
+// }
 
-console.log("app.js included");
+// console.log("app.js included");
